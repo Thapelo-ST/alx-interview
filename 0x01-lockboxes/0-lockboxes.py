3b262404b397267eb4cca029c4a7c95942161c87
Thapelo-ST <tsamkelo96@gmail.com>
2024-01-12 01:56:42 +0000

    Locked

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python3
-""" 
-lockboxes method, pass positive 
-intergers unless you will get a false or an error
-"""
+#!/usr/bin/python3
+"""lockboxes method, pass positive intergers or get an error"""
 from collections import deque
 
 
 def canUnlockAll(boxes):
-    """ 
-    lockboxes method, pass positive 
-    intergers unless you will get a false or an error
-    """
+    """lockboxes method, pass positive intergers or get an error"""
     n = len(boxes)
 
     opened_boxes = set()
