ae5724dce5f040d248a54251e4b014cdc594578a
Thapelo_ST <tsamkelo96@gmail.com>
2024-02-09 02:47:58 +0200

    fix 1

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,78 +1,61 @@
 #!/usr/bin/env python3
-"""module that solves the nQueens problem"""
 import sys
 
-"""checks if its safe"""
+class NQueensSolver:
+    def __init__(self, n):
+        self.n = n
+        self.board = [[0 for _ in range(n)] for _ in range(n)]
 
+    def is_safe(self, row, col):
+        for i in range(col):
+            if self.board[row][i] == 1:
+                return False
 
-def is_safe(board, row, col, n):
-    """checks if its safe"""
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if self.board[i][j] == 1:
+                return False
 
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+        for i, j in zip(range(row, self.n, 1), range(col, -1, -1)):
+            if self.board[i][j] == 1:
+                return False
 
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-"""solves the problem utilization"""
-
-
-def solve_nqueens_util(board, col, n):
-    """solves the problem utilization"""
-    if col == n:
-        print_solution(board, n)
         return True
 
-    res = False
-    for i in range(n):
-        if is_safe(board, i, col, n):
-            board[i][col] = 1
+    def solve_nqueens_util(self, col):
+        if col == self.n:
+            self.print_solution()
+            return True
 
-            res = solve_nqueens_util(board, col + 1, n) or res
+        res = False
+        for i in range(self.n):
+            if self.is_safe(i, col):
+                self.board[i][col] = 1
 
-            board[i][col] = 0
-    return res
+                res = self.solve_nqueens_util(col + 1) or res
 
+                self.board[i][col] = 0
 
-"""solves the nqueens problem"""
+        return res
 
+    def solve_nqueens(self):
+        if not isinstance(self.n, int):
+            print("N must be a number")
+            sys.exit(1)
 
-def solve_nqueens(n):
-    """solves the nqueens problem """
-    if not isinstance(n, int):
-        print("N must be a number")
-        sys.exit(1)
+        if self.n < 4:
+            print("N must be at least 4")
+            sys.exit(1)
 
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+        if not self.solve_nqueens_util(0):
+            print(f"No solution for N = {self.n}")
 
-    board = [[0 for _ in range(n)] for _ in range(n)]
-
-    if not solve_nqueens_util(board, 0, n):
-        print(f"No solution for N = {n}")
-
-
-"""printing function"""
-
-
-def print_solution(board, n):
-    """printing function"""
-    solution = []
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == 1:
-                solution.append([i, j])
-    print(solution)
-
+    def print_solution(self):
+        solution = []
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.board[i][j] == 1:
+                    solution.append([i, j])
+        print(solution)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -81,7 +64,8 @@ if __name__ == "__main__":
 
     try:
         N = int(sys.argv[1])
-        solve_nqueens(N)
+        solver = NQueensSolver(N)
+        solver.solve_nqueens()
     except ValueError:
         print("N must be a number")
         sys.exit(1)
